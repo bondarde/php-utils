b4de4c451cfb0657710a20a1e8118a2fc534858b
Alexander Bondar <alexander@bondar.de>
2020-12-20 15:54:12 +0100

    Add Arrays util

---
+++ src/Support/Arrays.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Bond211\Utils\Support;
+
+class Arrays
+{
+    public static function flattenKeys(array $arr, string $keyGlue = '.', string $keyPrefix = ''): array
+    {
+        $res = [];
+
+        foreach ($arr as $key => $val) {
+            if ($keyPrefix) {
+                $key = $keyPrefix . $keyGlue . $key;
+            }
+
+            if (gettype($val) === 'array') {
+                $tmp = self::flattenKeys($val, $keyGlue, $key);
+                $res = array_merge($res, $tmp);
+            } else {
+                $res[$key] = $val;
+            }
+        }
+
+        return $res;
+    }
+}
---
+++ tests/Support/ArraysTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Support;
+
+use Bond211\Utils\Support\Arrays;
+use PHPUnit\Framework\TestCase;
+
+class ArraysTest extends TestCase
+{
+    const FLATTEN_KEYS_INPUT = [
+        'key1' => 'val1',
+        'key2' => [
+            'key3' => 'val3',
+            'key4' => 'val4',
+            'key5' => [
+                'key6' => 'val6',
+            ],
+        ],
+    ];
+
+    public function testFlattenKeysWithDefaultSeparator()
+    {
+        $res = Arrays::flattenKeys(self::FLATTEN_KEYS_INPUT);
+
+        $expected = [
+            'key1' => 'val1',
+            'key2.key3' => 'val3',
+            'key2.key4' => 'val4',
+            'key2.key5.key6' => 'val6',
+        ];
+
+        self::assertEquals($expected, $res);
+    }
+
+    public function testFlattenKeysWithCustomSeparator()
+    {
+        $res = Arrays::flattenKeys(self::FLATTEN_KEYS_INPUT, '_');
+
+        $expected = [
+            'key1' => 'val1',
+            'key2_key3' => 'val3',
+            'key2_key4' => 'val4',
+            'key2_key5_key6' => 'val6',
+        ];
+
+        self::assertEquals($expected, $res);
+    }
+
+    public function testFlattenKeysWithKeyPrefix()
+    {
+        $res = Arrays::flattenKeys(self::FLATTEN_KEYS_INPUT, '.', 'config');
+
+        $expected = [
+            'config.key1' => 'val1',
+            'config.key2.key3' => 'val3',
+            'config.key2.key4' => 'val4',
+            'config.key2.key5.key6' => 'val6',
+        ];
+
+        self::assertEquals($expected, $res);
+    }
+}
